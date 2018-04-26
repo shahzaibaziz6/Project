dc54a718abbbcc4830ebda163db42353364b17ee
shahzaibaziz6 <shahzaibaziz6@gmail.com>
2018-04-27 00:46:18 +0500

    Send and Receive Activity starts

--- app/src/main/java/com/shahzaibaziz/filehub/SplashActivity.java
+++ app/src/main/java/com/shahzaibaziz/filehub/SplashActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -12,6 +13,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Toast.makeText(this,"Welcome to File Hub",Toast.LENGTH_SHORT).show();
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
