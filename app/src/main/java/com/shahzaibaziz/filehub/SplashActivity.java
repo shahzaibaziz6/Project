39e4b48712440be520310b7109c5ba52b07517f6
shahzaibaziz6 <shahzaibaziz6@gmail.com>
2018-05-06 21:20:23 +0500

    Start P2P connection

--- app/src/main/java/com/shahzaibaziz/filehub/SplashActivity.java
+++ app/src/main/java/com/shahzaibaziz/filehub/SplashActivity.java
@@ -4,16 +4,26 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private ImageView  ivSplash;
+    private ProgressBar pbSplash;
     private  static final int SPLASH_DISPLAY_LENGTH=2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_splash);
-        Toast.makeText(this,"Welcome to File Hub",Toast.LENGTH_SHORT).show();
+//        getActionBar().hide();
+        ivSplash= findViewById(R.id.iv_splash_background);
+        pbSplash = findViewById(R.id.pb_splash_welcome);
+        ivSplash.animate().alpha(0).setDuration(5000);
+        pbSplash.animate().scaleY(0.5f).scaleX(0.5f).setDuration(3000);
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
